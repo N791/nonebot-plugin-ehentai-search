31ac91cce26e48a18e1c622d7e9a188df9068261
n791 <a15345506127@163.com>
2024-08-15 14:04:01 +0800

    update pyproject

--- nonebot_plugin_ehentai_search/__init__.py
+++ nonebot_plugin_ehentai_search/__init__.py
@@ -1,4 +1,4 @@
-from nonebot import on_command, on_regex
+from nonebot import get_driver, on_command, on_regex
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import PluginMetadata 
 from nonebot.matcher import Matcher
@@ -12,10 +12,9 @@ from nonebot.adapters.onebot.v11 import (
     MessageSegment
 )
 
-import requests
+import httpx
 
-from io import BytesIO
-from selenium import webdriver
+from playwright.async_api import async_playwright
 from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
@@ -35,23 +34,16 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters={"~onebot.v11"}
 )
 
-# 创建Chrome参数对象
-options = webdriver.ChromeOptions()
+driver = get_driver()
 
-# 添加试验性参数
-options.add_experimental_option('excludeSwitches', ['enable-automation'])
-options.add_experimental_option('useAutomationExtension', False)
-
-#无头模式（隐藏浏览器界面）
-options.add_argument('--headless')
-browser = webdriver.Chrome(options=options)
-
-# 执行Chrome开发者协议命令（在加载页面时执行指定的JavaScript代码）
-browser.execute_cdp_cmd(
-    'Page.addScriptToEvaluateOnNewDocument',
-    {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'}
-)
-
+@driver.on_startup
+async def startup():
+    global p,browser,context
+    p = await async_playwright().start()
+    browser = await p.chromium.launch(headless=True)
+    context = await browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    )
 
 FCATS = {
     "NULL":0,
@@ -214,53 +206,52 @@ async def search_keyword(bot: Bot, matcher: Matcher, event: GroupMessageEvent, k
     await ehbz_search.finish()
     
 async def search(bot, matcher, event, resp_str) -> None:
-    global success_type,browser
+    global success_type
     # 获取页面源代码并处理
     try:
-        browser.get(resp_str)
+        page = await context.new_page()
+        await page.goto(resp_str)
     except Exception as e:
-            success_type = True
-            await matcher.finish(
-                message=f"搜索网站请求失败，错误信息{repr(e)}"
-            )
-    resp = browser.page_source
+        success_type = True
+        await matcher.finish(
+            message=f"搜索网站请求失败，错误信息{repr(e)}"
+        )
+    resp = await page.content()
     resp_obj = BeautifulSoup(resp,"html.parser")
     metadata_str = metadata.get_metadata(resp_obj,limit_num)
     logger.info("已获取到metadata")
     msgs = []
     i = 0
-    for meta_item in metadata_str:
-        i = i + 1
-        logger.info(f"已获取到结果{i}")
-        url_str = f"https://e-hentai.org/gallerytorrents.php?gid={meta_item['gid']}&t={meta_item['token']}"
-        try:
-            browser.get(url_str)   
-        except Exception as e:
-            success_type = True
-            await matcher.finish(
-                message=f"下载网站请求失败，错误信息{repr(e)}"
-            )
-        meta_item_url = browser.page_source
-        meta_link = BeautifulSoup(meta_item_url, "html.parser")
-        torrent_links = [link["href"] for link in meta_link.find_all("a", href=lambda href: href and href.endswith(".torrent"))]
-        re = requests.get(
-            url=meta_item['thumb'], 
-            timeout=120,
-        )
-        result_str = (f"标题:{meta_item['title']}\n"
+    async with httpx.AsyncClient() as client:
+        for meta_item in metadata_str:
+            i = i + 1
+            logger.info(f"已获取到结果{i}")
+            url_str = f"https://e-hentai.org/gallerytorrents.php?gid={meta_item['gid']}&t={meta_item['token']}"
+            try:
+                await page.goto(url_str)   
+            except Exception as e:
+                success_type = True
+                await matcher.finish(
+                    message=f"下载网站请求失败，错误信息{repr(e)}"
+                )
+            meta_item_url = await page.content()
+            meta_link = BeautifulSoup(meta_item_url, "html.parser")
+            torrent_links = [link["href"] for link in meta_link.find_all("a", href=lambda href: href and href.endswith(".torrent"))]
+            re = await client.get(meta_item['thumb'], timeout=120) #获取图片
+            result_str = (f"标题:{meta_item['title']}\n"
                 +f"类型:{meta_item['category']}\n"
                 +f"页数:{meta_item['filecount']}\n"
                 +f"标签:{', '.join(str(item) for item in meta_item['tags'])}\n"
                 +f"eh链接:https://e-hentai.org/g/{meta_item['gid']}/{meta_item['token']}/\n"
                 +f"磁力文件:(没显示出来则没有)\n"+'\n'.join(str(link_item) for link_item in torrent_links))
-        try:
-            image = Image.open(BytesIO(re.content))  # 打开图片
-            res_img = metadata.change_pixel(image,100)  # 修改图片以防风控
-            result = Message(result_str) + MessageSegment.image(res_img)
-        except Exception as e:
-            result = Message(result_str)
+            try:
+                image = Image.open(BytesIO(re.content))  # 打开图片
+                res_img = metadata.change_pixel(image,100)  # 修改图片以防风控
+                result = Message(result_str) + MessageSegment.image(res_img)
+            except Exception as e:
+                result = Message(result_str)
         
-        msg = {
+            msg = {
                 "type": "node",
                 "data": {
                     "name": "search_bot",
@@ -268,16 +259,17 @@ async def search(bot, matcher, event, resp_str) -> None:
                     "content": result,
                     },
                 }
-        msgs.append(msg)
+            msgs.append(msg)
     
-    try:
-        await bot.call_api(
+        try:
+            await bot.call_api(
                             "send_group_forward_msg",
                             group_id=event.group_id,
                             messages=msgs,
                         )
-    except Exception as e:
+        except Exception as e:
             success_type = True
             await matcher.finish(
                 message=f"消息可能被风控了，发不出来，错误信息{repr(e)}"
             )
+            
